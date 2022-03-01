baa048a4ff6f84f74ad543bfb4c082220333f07e
Caturra <wasdjkl233@gmail.com>
2022-03-01 21:27:41 +0800

    test co::test()

--- test.cpp
+++ test.cpp
@@ -3,16 +3,25 @@
 #include <vector>
 #include "Coroutine.h"
 #include "Environment.h"
+#include "Utilities.h"
+
+void where() {
+    std::cout << "running code in a "
+              << (co::test() ? "coroutine" : "thread")
+              << std::endl;
+}
 
 void print1() {
     std::cout << 1 << std::endl;
-    co::Coroutine::yield();
+    co::this_coroutine::yield();
     std::cout << 2 << std::endl;
 }
 
 void print2(int i, co::Coroutine *co) {
     std::cout << i << std::endl;
     co->resume();
+    where();
+    std::cout << "bye" << std::endl;
 }
 
 int main() {
@@ -21,5 +30,6 @@ int main() {
     auto co2 = env.createCoroutine(print2, 3, co.get());
     co->resume();
     co2->resume();
+    where();
     return 0;
 }
